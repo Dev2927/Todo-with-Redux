6ae97bc840aab15bc4e95d6c5dd17304d3e9a835
Dev anand <devanand290702@gmail.com>
2023-09-27 00:21:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Todo with Redux</h1>
+     <AddTodo />
+     <Todos />
+    </>
+  )
+}
+
+export default App
